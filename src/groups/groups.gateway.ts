b68d012edc11a04e1ef244cec694d6e3bd838657
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-04-10 00:48:06 +0100

    added chat

--- src/groups/groups.gateway.ts
+++ src/groups/groups.gateway.ts
@@ -1,6 +1,7 @@
 import { OnGatewayConnection, OnGatewayDisconnect,
    SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { SocketInterface } from 'src/chat/interface';
 
 @WebSocketGateway({ namespace: '/group' })
 export class GroupsGateway implements OnGatewayConnection, OnGatewayDisconnect  {
@@ -9,19 +10,25 @@ export class GroupsGateway implements OnGatewayConnection, OnGatewayDisconnect  
   users = 0
   
   async handleConnection() {
-    // A client has connected
-    this.users++;
-    // Notify connected clients of current users
-    this.server.emit('users', this.users);
+    // A client has connected authenticate and add to group
+
   }
   async handleDisconnect() {
-    // A client has disconnected
-    this.users--;
-    // Notify connected clients of current users
+    // send connected users too
     this.server.emit('users', this.users);
   }
-  @SubscribeMessage('chat')
-  async onChat(client, message) {
-    client.broadcast.emit('chat', message);
+  @SubscribeMessage('group:join')
+  async onJoin(client: SocketInterface, room: string) {
+    client.join(room);
+    client.to(room).emit(`${client.username} joined`)
+  }
+
+  @SubscribeMessage('group:chat')
+  async onChat(client: SocketInterface, message:string) {
+
+    const payload  = JSON.parse(message)
+
+    if (payload.room in client.rooms)
+          client.to(payload.room).emit('group:chat', message);
   }
 }
