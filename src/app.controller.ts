af77926ebdbf103e4562b388dd43f250e45c316e
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-04-15 17:33:59 +0100

    added server sent event

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { RedislibService } from '@app/redislib';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private redisService: RedislibService,
+    private appService
+    ) {
+    this.redisService.subscribe('notifications')
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('sse')
+  async sse(): Promise<Observable<string>> {
+
+    try {
+      const message: string = await this.redisService.onReceive('notifications')
+      return new Observable((observable) => {
+        observable.next(message)
+      })
+    } catch (error) {
+      console.log(error)
+      
+    }
   }
 }
